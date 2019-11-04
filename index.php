58334e4fb9bdd90ef60de4aa0cbacc70c373f632
= <=>
2019-11-04 15:55:53 +0500

    Documentation file for porject

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Simple Sidebar - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+
+  <!-- Custom styles for this template -->
+
+
+</head>
+
+<style type="text/css">
+  *{
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: Geneva, Tahoma, Verdana, sans-serif;
+  margin: 0px;
+}
+
+.nav{
+  width: 100%;
+  height: 50px;
+    background: rgba(200,200,200,0.2);
+    position: fixed;
+    top: 0px;
+}
+
+#menu{
+  float: left;
+    font-size: 40px;
+    font-weight: normal;
+    padding-right: 20px;
+    color: rgba(100,100,100,0.9);;
+}
+
+.nav ul{
+  width: 150px;
+  list-style-type: none;
+  background: rgba(200,200,200,0.2);
+    border-radius: 5px;
+}
+
+#top-menu{
+   clear: both;
+   display: block;
+   float: left;
+   padding-left: 0px;
+   margin-top: 10px;
+   margin-right: 20px;
+
+}
+.links{
+  color: black;
+  font-size: 20px;
+  font-weight: normal;
+}
+
+.nav ul li a{
+  text-decoration: none;
+  text-align: center;
+  display: block;
+  padding: 2px 0px 2px 10px;
+}
+.nav ul li a:hover{
+  color: #fff;
+  background-color: black;
+  /*border-left: thick solid red;*/
+}
+
+#wrapper{
+  margin-top: 50px;
+}
+
+.container{
+  width: 800px;
+  height: 750px;
+  margin: 0px auto;
+    background: rgba(200,200,200,0.2);
+    border-bottom: 1px solid rgba(200,200,200,1);
+    padding: 20px;
+}
+
+.active{
+  color: white;
+  background-color: rgba(100,100,100,1);
+  /*border-left: thick solid rgba(0,255,0,0.5);*/
+}
+</style>
+<body>
+
+  <div class="nav">
+    <span id ="menu">&#9776;</span> 
+    <ul id="top-menu">
+      <li><a href="#one" class="links">Menu 1</a></li>
+      <li><a href="#two" class="links">Menu 2</a></li>
+      <li><a href="#three" class="links">Menu 3</a></li>
+      <li><a href="#four" class="links">Menu 4</a></li>
+      <li><a href="#five" class="links">Menu 5</a></li>
+      <li><a href="#six" class="links">Menu 6</a></li>
+    </ul>
+  </div>
+  <div id="wrapper" >
+    <div id="one" class="container">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+    <div id="two" class="container">
+      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    </div>
+    <div id="three" class="container">
+      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+    </div>
+    <div id="four" class="container">
+      There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+    </div>
+    <div id="five" class="container">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pellentesque orci non lacus facilisis, a egestas massa aliquet. Nullam finibus mi a velit malesuada luctus. Interdum et malesuada fames ac ante ipsum primis in faucibus. In id lobortis ligula, a tempus dui. In quis eros et orci iaculis consectetur. Fusce sed aliquam quam. Suspendisse ut neque ac nibh bibendum porta. Nulla tempor dui ac luctus lacinia. Maecenas vestibulum posuere ligula, ac tincidunt tortor ullamcorper a. Mauris feugiat id orci sit amet luctus. Praesent fringilla ultrices ex. Phasellus lectus leo, viverra vitae ultricies quis, blandit a odio. Nunc rutrum mauris et gravida efficitur. Quisque nec dolor odio. Fusce sit amet erat ut sapien semper tristique. Aenean mollis libero sed lorem semper ullamcorper.
+
+Proin viverra erat eget imperdiet tristique. Maecenas sed diam ornare, aliquam dui ut, mattis leo. Quisque sit amet tortor id nisi ultrices interdum et ac turpis. Sed laoreet imperdiet ante et accumsan. Phasellus interdum ullamcorper nibh, et elementum enim auctor eu. Fusce mattis erat sapien, vitae sollicitudin neque fermentum et. Aliquam mollis eros vitae faucibus ultrices.
+
+Praesent semper tortor non justo commodo euismod. Nulla facilisi. Fusce porta fringilla lorem at tempor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam eleifend tincidunt ante, quis suscipit odio accumsan eget. Nunc sodales enim quis auctor auctor. Morbi sem nulla, pretium at placerat at, convallis at mauris. Nunc consequat mauris id malesuada tincidunt. Donec ac ante a tortor ullamcorper commodo. Fusce egestas nec sapien sit amet feugiat. Curabitur sit amet lacus quis sapien luctus malesuada.
+
+Mauris id libero vel ante mollis fringilla hendrerit cursus turpis. Integer eu purus convallis, egestas risus at, sagittis ligula. Aliquam rhoncus tortor sed condimentum cursus. In non tincidunt eros. Vivamus euismod massa dapibus, lobortis justo quis, condimentum nisl. Donec nec ligula id est aliquet elementum sagittis eu ex. Suspendisse nec ipsum lacus. Morbi id urna vitae sem commodo accumsan. Pellentesque quam nisl, elementum egestas porta eget, gravida quis eros. Nunc eu neque nisl. Phasellus urna augue, commodo sit amet ornare eu, blandit semper tortor. Duis eget feugiat ex. Quisque a risus nec turpis consectetur lacinia.
+
+Cras sed magna tortor. Nunc nisi leo, gravida in fermentum ac, ornare a mi. Mauris auctor laoreet lacus. Mauris eu dui id mauris pretium consectetur ac et justo. Aliquam erat volutpat. Nullam ultrices neque vitae vehicula fermentum. Suspendisse pretium fringilla diam vitae efficitur. Curabitur eu urna sit amet tortor lobortis tincidunt a id lectus. Suspendisse quis dapibus lorem. Vivamus tristique ipsum non auctor fermentum. Vivamus nunc quam, dictum ut elit sit amet, rutrum porttitor dui. Cras interdum ante sit amet tortor placerat, at iaculis dolor consectetur.
+    </div>
+    <div id="six" class="container">
+      Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    </div>
+  </div>
+
+</body>
+  <!-- /#wrapper -->
+
+  <!-- Bootstrap core JavaScript -->
+  
+
+  <!-- Menu Toggle Script -->
+  <script>
+   $(document).ready(function(){
+  // $sections incleudes all of the container divs that relate to menu items.
+  var $sections = $('.container');
+  
+  // The user scrolls
+  $(window).scroll(function(){
+    
+    // currentScroll is the number of pixels the window has been scrolled
+    var currentScroll = $(this).scrollTop();
+    
+    // $currentSection is somewhere to place the section we must be looking at
+    var $currentSection
+    
+    // We check the position of each of the divs compared to the windows scroll positon
+    $sections.each(function(){
+      // divPosition is the position down the page in px of the current section we are testing      
+      var divPosition = $(this).offset().top;
+      
+      // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
+      // the -1 is so that it includes the div 1px before the div leave the top of the window.
+      if( divPosition - 1 < currentScroll ){
+        // We have either read the section or are currently reading the section so we'll call it our current section
+        $currentSection = $(this);
+         var id = $currentSection.attr('id');
+      // alert(id)
+      // document.write(id)
+     $('a').removeClass('active');
+     $("[href=#"+id+"]").addClass('active');
+        // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+      }
+      
+      // This is the bit of code that uses the currentSection as its source of ID
+     
+      
+    })
+
+  });
+});
+  </script>
+</html>
